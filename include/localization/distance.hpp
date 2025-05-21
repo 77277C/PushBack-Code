65d4c3da6eddf67947a8be5f591d1dedae62dae9
TheMaster3558 <chawk_k@outlook.com>
2025-05-20 22:52:10 -0700

    Add more

--- include/localization/distance.hpp
+++ include/localization/distance.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <Eigen/Core>
+#include <Eigen/Geometry>
 #include <pros/distance.hpp>
 
 
 constexpr double MM_TO_IN = 1 / 25.4;
-constexpr double THRESHOLD = 393.0;
 
 
 class Distance {
@@ -19,9 +19,15 @@ public:
     }
 
     [[nodiscard]] std::optional<double> get_probability(const Eigen::Vector3f& X) const {
-        if (distance > THRESHOLD) {
+        if (distance > 9999 * MM_TO_IN) {
             return std::nullopt;
         }
+
+        // Get the current absolute angle of the sensor (robot angle + offset angle)
+        auto angle = X.z() + offset.z();
+
+        // Get the current absolute position of the sensor (robot position + offset relative to field)
+        Eigen::Vector2f x = X.head<2>() + Eigen::Rotation2Df(X.z()) * offset.head<2>();
     }
 
 private:
